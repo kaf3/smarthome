dd411397b117aa40264f51ed437fead6d0d55404
Naumov Nikita <nekit.97@bk.ru>
2020-05-04 17:57:41 +0300

    feat: ci

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
+    transform: {
+        '^.+\\.(ts|js|html)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+}
