c6d4077788d6488caf68dd893bbf48031142734e
Naumov Nikita <nekit.97@bk.ru>
2020-05-04 19:19:09 +0300

    test4

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,7 +1,6 @@
 import {ActionReducerMap} from '@ngrx/store';
 import {routerReducer} from '@ngrx/router-store';
 import {AppState} from './state';
-import {EntityMetadataMap} from '@ngrx/data';
 
 export const appReducers: Partial<ActionReducerMap<AppState, any>> = {
     // не оч понятно сюда добавлять всегда всех редюсеров? и как пофиксить варн без any в дженерике
