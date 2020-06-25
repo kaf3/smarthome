cd309836eaa2f1e4ebf5e7f83ad289e0b9d8bb5e
Naumov Nikita <nekit.97@bk.ru>
2020-06-25 16:11:56 +0300

    ui command

--- src/app/ui/commands/commands.component.ts
+++ src/app/ui/commands/commands.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
 	templateUrl: './commands.component.html',
 	styleUrls: ['./commands.component.scss'],
 })
-export class CommandsComponent {}
+export class CommandsComponent {
+	comparisons = [
+		{
+			sign: '>',
+			label: 'больше',
+		},
+		{
+			sign: '<',
+			label: 'меньше',
+		},
+		{
+			sign: '=',
+			label: 'равно',
+		},
+	];
+}
--- src/app/ui/commands/commands.module.ts
+++ src/app/ui/commands/commands.module.ts
@@ -3,9 +3,23 @@ import { CommonModule } from '@angular/common';
 import { CommandsComponent } from './commands.component';
 import { CommandsRoutingModule } from './commands.routing.module';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { NgrxMaterialModule } from '@helpers';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [CommandsComponent],
-	imports: [CommonModule, CommandsRoutingModule, MatCardModule],
+	imports: [
+		CommonModule,
+		CommandsRoutingModule,
+		MatCardModule,
+		MatExpansionModule,
+		MatListModule,
+		NgrxMaterialModule,
+		MatButtonModule,
+		MatIconModule,
+	],
 })
 export class CommandsModule {}
