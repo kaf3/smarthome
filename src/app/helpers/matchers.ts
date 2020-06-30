399414dc2b6ee0ae78e63a7f5c71e80feb74f3c6
Naumov Nikita <nekit.97@bk.ru>
2020-06-30 19:49:14 +0300

    command processor

--- src/app/helpers/matchers.ts
+++ src/app/helpers/matchers.ts
@@ -1,19 +1,5 @@
-import { Room } from '@models/room';
-import { Hardware } from '@models/hardware';
-import { Equipment } from '@models/equipment';
-import { Command } from '@models/command';
-
-export interface Ids {
-	roomId: Room['id'];
-	hardwareId: Hardware['id'];
-	equipmentId: Equipment['id'];
-}
-
 export class Matchers {
-	private static pushId = '[\\w-]{20}';
-	private static eqpPath = `${Matchers.pushId}\\*${Matchers.pushId}\\*[\\w-]{4}`;
-	private static bodyEvent = `${Matchers.eqpPath}\\*[<>=]\\*\\w+`;
-	private static bodyResult = `${Matchers.eqpPath}\\*\\d`;
+	public static pushId = '[\\w-]{20}';
 
 	static urlRoomMatcher(url: string): boolean {
 		return new RegExp(`^/rooms/${Matchers.pushId}$`).test(url);
@@ -22,27 +8,6 @@ export class Matchers {
 
 	static urlRoomsMatcher(url: string): boolean {
 		return /^\/rooms$/.test(url);
-	}
-
-	static commandBodyMatcher(body: Command['body']): boolean {
-		return !!body && new RegExp(`^#${Matchers.bodyEvent}\\*${Matchers.bodyResult}$`).test(body);
-	}
-
-	static bodyParser(body: Command['body']): { event: Ids; result: Ids } {
-		const event = (new RegExp(Matchers.bodyEvent).exec(body ?? '') ?? [''])[0].split('*');
-		const result = (new RegExp(Matchers.bodyResult).exec(body ?? '') ?? [''])[0].split('*');
-		return {
-			event: {
-				roomId: event[0],
-				hardwareId: event[1],
-				equipmentId: event[2],
-			},
-			result: {
-				roomId: result[0],
-				hardwareId: result[1],
-				equipmentId: result[2],
-			},
-		};
 	}
 
 	//#-MAN02LibdHPPTcgFeRZ*-MANBXXGr44pbIr4nafo*co2-*>*50*-MAN3OCDv10jwbNeIWq_*-MANCAM4zwH3387f8bC2*outl*0
--- src/app/models/command/command.ts
+++ src/app/models/command/command.ts
@@ -1,5 +1,82 @@
 import { BaseDomain, OmitByPropType } from '@models/common';
 import { Matchers } from '@helpers';
+import { Equipment } from '@models/equipment';
+import { Room } from '@models/room';
+import { Hardware } from '@models/hardware';
+
+export type Comparator = '>' | '<' | '=';
+export type CommandType = 'byEvent';
+
+export interface CommandObject {
+	equipmentId: Equipment['id'];
+	roomId: Room['id'];
+	hardwareId: Hardware['id'];
+}
+
+export interface CommandSensor extends CommandObject {
+	value: Equipment['value'];
+	comparator: Comparator;
+}
+
+export interface CommandDevice extends CommandObject {
+	value: Equipment['value'];
+}
+
+export interface CommandBody {
+	trigger: CommandSensor | number;
+	result: CommandDevice;
+}
+
+export class CommandProcessor {
+	private static eqpPath = `${Matchers.pushId}\\*${Matchers.pushId}\\*[\\w-]{4}`;
+	private static bodyEventSensor = `${CommandProcessor.eqpPath}\\*[<>=]\\*\\w+`;
+	private static bodyResult = `${CommandProcessor.eqpPath}\\*\\d`;
+
+	static commandBodyMatcher(body: CommandDTO['body']): boolean {
+		return !!body && new RegExp(`^#${this.bodyEventSensor}\\*${this.bodyResult}$`).test(body);
+	}
+
+	static bodyParser(body: CommandDTO['body'], type: CommandType): Command['body'] {
+		let trigger: CommandBody['trigger'];
+		if (type === 'byEvent') {
+			const sensorData = (new RegExp(this.bodyEventSensor).exec(body ?? '') ?? [''])[0].split(
+				'*',
+			);
+			trigger = {
+				roomId: sensorData[0],
+				hardwareId: sensorData[1],
+				equipmentId: sensorData[2],
+				comparator: sensorData[3] as Comparator,
+				value: sensorData[4],
+			};
+		} else {
+			trigger = 0;
+		}
+		const result = (new RegExp(this.bodyResult).exec(body ?? '') ?? [''])[0].split('*');
+		return {
+			trigger,
+			result: {
+				roomId: result[0],
+				hardwareId: result[1],
+				equipmentId: result[2],
+				value: result[3],
+			},
+		};
+	}
+
+	static bodySerializer(body: Command['body'], type: CommandType): CommandDTO['body'] {
+		let trigger: string;
+		if (type === 'byEvent') {
+			const triggerObj = body?.trigger as CommandSensor;
+			trigger = `#${triggerObj.roomId}*${triggerObj.hardwareId}*${triggerObj.equipmentId}*${triggerObj.comparator}*${triggerObj.value}`;
+		} else trigger = '0';
+		const result = `${body?.result.roomId}*${body?.result.hardwareId}*${
+			body?.result.equipmentId
+		}*${!!body?.result.value ? '1' : '0'}`;
+
+		return trigger + '*' + result;
+	}
+}
 
 export type CommandDTOProps = OmitByPropType<CommandDTO, Function>;
 
@@ -21,24 +98,17 @@ export class CommandDTO {
 }
 
 export class Command extends BaseDomain {
-	public readonly body: string | null;
+	public readonly body: CommandBody | null;
 
 	constructor(source: Command) {
 		super(source.id, source.name);
-		const body = source.body;
-		if (body === null || Matchers.commandBodyMatcher(body)) {
-			this.body = body;
-		} else {
-			throw new Error(`Command ${source.name} has incorrect body ${source.body}`);
-		}
-		this.body = body;
-		Matchers.bodyParser(body);
+		this.body = source.body;
 	}
 
 	public static createDTO(command: Command): CommandDTO {
 		return new CommandDTO({
 			name: command.name,
-			body: command.body,
+			body: CommandProcessor.bodySerializer(command.body, 'byEvent'),
 		});
 	}
 
@@ -51,7 +121,7 @@ Object.defineProperty(CommandDTO.prototype, 'createDomain', {
 		return new Command({
 			id,
 			name: this.name,
-			body: this.body,
+			body: CommandProcessor.bodyParser(this.body, 'byEvent'),
 		});
 	},
 });
