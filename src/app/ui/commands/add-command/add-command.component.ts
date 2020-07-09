8cfa9896a24f01a8a385ebc471fb6c5b07575375
Naumov Nikita <nekit.97@bk.ru>
2020-07-10 01:18:49 +0300

    feat: add command done success

--- src/app/ui/commands/add-command/add-command.component.ts
+++ src/app/ui/commands/add-command/add-command.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { RoomListFacade } from '@store/room-list';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { Room } from '@models/room';
 import { Hardware } from '@models/hardware';
 import { Equipment } from '@models/equipment';
 import { Command, CommandBody } from '@models/command';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
-import { CommandListFacade } from '@store/command-list';
+import {
+	AbstractControl,
+	AsyncValidatorFn,
+	FormControl,
+	FormGroup,
+	ValidationErrors,
+	Validators,
+} from '@angular/forms';
+import { CommandListFacade, CommandListStoreActions } from '@store/command-list';
+import { BaseDomain } from '@models/common';
+import { catchError, debounceTime, map, take, takeUntil } from 'rxjs/operators';
+import { MatHorizontalStepper } from '@angular/material/stepper';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Actions, ofType } from '@ngrx/effects';
 
 export interface FullAboutEquipment {
 	room: Room;
@@ -14,31 +26,48 @@ export interface FullAboutEquipment {
 	hardware: Hardware;
 }
 
+const notUnique: ValidationErrors = { notUnique: true };
+
 @Component({
 	selector: 'app-add-command',
 	templateUrl: './add-command.component.html',
 	styleUrls: ['./add-command.component.scss'],
 })
-export class AddCommandComponent implements OnInit {
+export class AddCommandComponent implements OnInit, OnDestroy {
 	public rooms$: Observable<Room[]>;
 	public command: Command;
 	public aboutEventSensor: FullAboutEquipment;
 	public aboutResultDevice: FullAboutEquipment;
 	public eventForm = new FormGroup({
-		sensorValue: new FormControl('', Validators.required.bind(Validators)),
+		sensorValue: new FormControl('', [
+			Validators.required.bind(Validators),
+			Validators.pattern(/^\d+$/),
+		]),
 		comparator: new FormControl('', Validators.required.bind(Validators)),
+		name: new FormControl('', Validators.required.bind(Validators)),
+		chosenSensor: new FormControl('', Validators.required.bind(Validators)),
 	});
 	public resultForm = new FormGroup({
 		deviceValue: new FormControl(false, Validators.required.bind(Validators)),
+		chosenDevice: new FormControl('', Validators.required.bind(Validators)),
 	});
+	@ViewChild('stepper') public readonly stepper: MatHorizontalStepper;
+
+	public destroy$ = new Subject();
 
 	constructor(
 		private readonly roomListFacade: RoomListFacade,
 		private readonly commandListFacade: CommandListFacade,
+		private readonly dialogRef: MatDialogRef<AddCommandComponent>,
+		public readonly actions$: Actions,
 	) {}
 
 	ngOnInit(): void {
 		this.rooms$ = this.roomListFacade.rooms$;
+		this.nameControl?.setAsyncValidators(
+			this.notUniqueValidator<Command>(this.commandListFacade.commands$),
+		);
+		this.eventForm.statusChanges.subscribe(console.log);
 	}
 
 	get sensorValueControl(): AbstractControl | null {
@@ -49,8 +78,20 @@ export class AddCommandComponent implements OnInit {
 		return this.eventForm.get('comparator');
 	}
 
+	get nameControl(): AbstractControl | null {
+		return this.eventForm.get('name');
+	}
+
+	get chosenSensorControl(): AbstractControl | null {
+		return this.eventForm.get('chosenSensor');
+	}
+
 	get deviceValueControl(): AbstractControl | null {
 		return this.resultForm.get('deviceValue');
+	}
+
+	get chosenDeviceControl(): AbstractControl | null {
+		return this.resultForm.get('chosenDevice');
 	}
 
 	comparisons = [
@@ -74,11 +115,31 @@ export class AddCommandComponent implements OnInit {
 		hardware: Hardware,
 		equipment: Equipment,
 	): void {
-		if (stage === 'event') this.aboutEventSensor = { room, hardware, equipment };
-		if (stage === 'result') this.aboutResultDevice = { room, hardware, equipment };
+		if (stage === 'event') {
+			this.aboutEventSensor = { room, hardware, equipment };
+			this.chosenSensorControl?.setValue(equipment.id);
+		}
+		if (stage === 'result') {
+			this.aboutResultDevice = { room, hardware, equipment };
+			this.chosenDeviceControl?.setValue(equipment.id);
+		}
+	}
+
+	notUniqueValidator<T extends BaseDomain>(entities$: Observable<T[]>): AsyncValidatorFn {
+		return function (control: AbstractControl): Observable<null | ValidationErrors> {
+			return entities$.pipe(
+				debounceTime(300),
+				take(1),
+				map((entities) =>
+					!!entities.find((ent) => ent.name === control.value) ? notUnique : null,
+				),
+				catchError(() => of(notUnique)),
+			);
+		};
 	}
 
 	addCommand(): void {
+		this.chosenDeviceControl?.markAsDirty();
 		if (
 			this.eventForm.valid &&
 			this.resultForm.valid &&
@@ -100,8 +161,30 @@ export class AddCommandComponent implements OnInit {
 					value: !!this.deviceValueControl?.value,
 				},
 			};
-
-			this.commandListFacade.addCommand(new Command({ name: 'Test', body, id: null }));
+			const name = this.nameControl?.value;
+			this.commandListFacade.addCommand(new Command({ name, body, id: null }));
+			this.checkAndClose(name);
 		}
+	}
+
+	checkAndClose(name: Command['name']): void {
+		this.actions$
+			.pipe(
+				ofType<CommandListStoreActions.AddCommandSuccess>(
+					CommandListStoreActions.CommandListActionTypes.AddCommandSuccess,
+				),
+				take(1),
+				takeUntil(this.destroy$),
+			)
+			.subscribe((action) => {
+				if (action.payload.command.name === name) {
+					this.dialogRef.close();
+				}
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next();
+		this.destroy$.complete();
 	}
 }
